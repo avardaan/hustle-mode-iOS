4bb2d52822f220d3d58953333e3573d68bbb65c9
Vardaan <vardaan119@gmail.com>
2018-06-20 17:19:14 -0400

    fin

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -17,9 +18,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var hustleLabel: UILabel!
     @IBOutlet weak var onLabel: UILabel!
     
+    // instance of an audio player
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // specify route to the audio file
+        let path = Bundle.main.path(forResource:"hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        // try loading player instance
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,11 +43,22 @@ class ViewController: UIViewController {
     
     // when Power Button is pressed
     @IBAction func powerButtonPressed(_ sender: UIButton) {
-        // these elements are hidden by default
-        // pressing the button would unhide them
+        // show cloud
+        // hide blueBG and button
         cloudHolder.isHidden = false
-        darkBlueBG.isHidden = false
-        powerButton.isHidden = false
+        darkBlueBG.isHidden = true
+        powerButton.isHidden = true
+        // play sound
+        player.play()
+        // rocket animation
+        UIView.animate(withDuration: 2.3, animations: {
+            // animate rocket to following position
+            self.rocket.frame = CGRect(x: 0, y: 250, width: 375, height: 200)
+        }) { (finished) in
+            // completion handler -> show labels
+            self.hustleLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
     }
     
 
